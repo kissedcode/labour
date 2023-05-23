e41c3db32bcf3242bc6b0d0023fd0f84f78e9625
kissed <kissed.code@ya.ru>
2023-05-23 19:42:12 +0300

    version 0.1.0.1

--- app/appAndroid/build.gradle.kts
+++ app/appAndroid/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "dev.kissed.labour"
 
         versionCode = 1
-        versionName = "0.1"
+        versionName = "0.1.0"
 
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
